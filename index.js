89dfbb8ece33ab55f746c185e8d90d77df7336d0
Artak Vardanyan <artakvg@gmail.com>
2017-07-19 18:35:57 +0400

    Fixed npm i, and npm compile

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-/**
- * Created by root on 7/11/17.
- */
-import { Node } from './src'
+'use strict';
 
-export {Node, nodeEvents, Server, Client} from './src'
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Client = exports.Server = exports.nodeEvents = exports.Node = undefined;
 
-export default Node;
+var _src = require('./src');
+
+Object.defineProperty(exports, 'Node', {
+  enumerable: true,
+  get: function get() {
+    return _src.Node;
+  }
+});
+Object.defineProperty(exports, 'nodeEvents', {
+  enumerable: true,
+  get: function get() {
+    return _src.nodeEvents;
+  }
+});
+Object.defineProperty(exports, 'Server', {
+  enumerable: true,
+  get: function get() {
+    return _src.Server;
+  }
+});
+Object.defineProperty(exports, 'Client', {
+  enumerable: true,
+  get: function get() {
+    return _src.Client;
+  }
+});
+exports.default = _src.Node;
